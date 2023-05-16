f90efbf5431760fee3ecdf387188a6817a6191e7
devexptester <devexperimenttester@gmail.com>
2023-05-17 00:51:43 +0900

    Project Commit
    Project commit frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { checkLogin } from './features/user/userSlice';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register'
+import Posting from './pages/Posting'
+import Verification from './pages/Verification';
+import Profile from './pages/Profile';
+import PostingDetails from './pages/PostingDetails';
+import NewPosting from './pages/NewPosting';
+import Navbar from './components/Navbar';
+import ResetPassword from './pages/ResetPassword';
+import ResetPasswordRequest from './pages/ResetPasswordRequest';
+import ResendVerification from './pages/ResendVerification';
+import EditProfile from './pages/EditProfile';
 
 function App() {
+  const dispatch = useDispatch()
+  const userToken = localStorage.getItem('user_token')
+  const userGlobal = useSelector((state) => state.user.user)
+
+  useEffect(() => {
+    if (userToken != '' && userToken != null && userToken != undefined) {
+      dispatch(checkLogin(userToken))
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='/login' element={<Login />} ></Route>
+        <Route path='/register' element={<Register />} ></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/profile/edit' element={<EditProfile />}></Route>
+        <Route path='/posting/' element={<Posting />} ></Route>
+        <Route path='/posting/new-posting' element={<NewPosting />} ></Route>
+        <Route path='/posting/:id/details' element={<PostingDetails />} ></Route>
+        <Route path='/users/verification/:token' element={<Verification />} />
+        <Route path='/users/reset-password/send-request' element={<ResetPasswordRequest />} />
+        <Route path='/users/reset-password/:token' element={<ResetPassword />} />
+        <Route path='/users/resend-verification' element={<ResendVerification />} />
+      </Routes>
     </div>
   );
 }
